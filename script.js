f688fd9a27ec5d41d3db93de70f69d3c5672f718
MichaelRP98 <142263291+MichaelRP98@users.noreply.github.com>
2023-11-16 22:45:11 -0500

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-function encenderMechero() {
+function agregarAgua() {
+    reiniciarSimulacion();
     document.getElementById('agua1').style.height = '80%';
+}
+
+function encenderMechero() {
     document.getElementById('vapor').style.display = 'block';
 }
 
 function pasarVapor() {
     document.getElementById('vapor').style.display = 'none';
+    document.getElementById('aguaDestilada').style.height = '80%';
+}
+
+function llenarAguaDestilada() {
+    reiniciarSimulacion();
     document.getElementById('aguaDestilada').style.height = '80%';
 }
